f9ad69580dd41bbc18b404a9c8dc2f52987dcc8a
shji <shji@linkprice.com>
2017-07-11 09:24:37 +0900

    주석 추가 및 Jarray 추가

--- sample/ASP/index.aspx.vb
+++ sample/ASP/index.aspx.vb
@@ -7,28 +7,31 @@ Public Class WebForm1
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        Dim test As New lpEncrypt("./public.pem")
+        Dim lpenc As New lpEncrypt("C:\Users\LG\Documents\Visual Studio 2017\Projects\WebApplication2\WebApplication2\public.pem")
 
-        'Dim lpInfo = Request.Cookies("LPINFO").Value
-        Dim jarray = New JArray()
-        Dim strArray() As String = {"mango", "melon"}
+        Dim lpInfo = Request.Cookies("LPINFO").Value
 
-        jarray.Add("apple")
-        jarray.Add("banana")
+        Dim p_nm = New JArray()				'여러상품 구매시 array을 이용하여 초기화 할수있습니다. 
+        Dim p_cd = New JArray()				'예)  p_nm_arr이라는 상품이름 array가 있을때
+        Dim c_cd = New JArray()				'     Dim p_nm = New JArray(p_nm_arr)
+        Dim sales = New JArray()
+        Dim it_cnt = New JArray()
 
-        'test.setJson("a_id", "lpInfo")
-        test.setJson("m_id", "lastsave4")
-        'test.setJson("mr_id", "lp_asp_test")
-        test.setJson("o_cd", "shji")
-        test.setJson("p_cd", String.Join("||", strArray))
-        test.setJson("it_cnt", 1)
-        test.setJson("sales", 100)
-        test.setJson("c_cd", "mobile")
-        'test.setJson("p_nm", "jordan11")
+        If lpInfo <> "" Then
+            lpenc.setJson("a_id", lpInfo)								'LPINFO 쿠키정보 (Cookie named LPINFO)
+            lpenc.setJson("m_id", merchant_id)						'머천트 ID(Merchant ID in Linkprice system)
+            lpenc.setJson("mbr_id", user_id)							'실적 발생 회원 ID(User ID who make business recorde in merchant system )
+            lpenc.setJson("o_cd", order_code)						'주문코드(Order number)
+            lpenc.setJson("p_cd", p_cd)								'상품코드(Product code)
+            lpenc.setJson("it_cnt", it_cnt)							'상품개수(Number of product)
+            lpenc.setJson("sales", sales)								'판매액(Sales amount)
+            lpenc.setJson("c_cd", c_cd)								'카테고릐 코드(Category code)
+            lpenc.setJson("p_nm", p_nm)								'상품이름	(Name of product)
+            lpenc.setJson("user_agent", Request.ServerVariables("HTTP_USER_AGENT"))		'접속 디바이스 정보(Device infomation)
+            lpenc.setJson("user_agent", Request.ServerVariables("REMOTE_ADDR"))				'실적 발생 회원 IP(User IP)
 
-        Response.Write(test.encrypt)
-
-        'test.submit()
+            lpenc.submit()
+        End If
 
     End Sub
 
