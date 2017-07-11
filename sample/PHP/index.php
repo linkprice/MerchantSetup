7de3ba0b28431207320024ede1cb9abb921ed374
shji <shji@linkprice.com>
2017-07-11 13:26:45 +0900

    쿠키 이름 변경

--- sample/PHP/index.php
+++ sample/PHP/index.php
@@ -5,7 +5,7 @@
 
 	$lpenc = new lpEncrypt("./public.pem");		//public.pem 경로 확인
 
-	$LPINFO = $_COOKIE[‘LPINFO’];
+	$LPINFO = $_COOKIE["LPINFO"];
 	
 	$p_nm_arr[] = $product_name;			//여럭 상품 구매시 arrayList를 통하여 한번에 넘겨줍니다.
 	$p_cd_arr[] = $product_code;
@@ -14,7 +14,7 @@
 	$it_cnt_arr[] = $item_count;
  
 	if(isset($LPINFO)) {
-		$lpenc->set("a_id", $cookie);					//LPINFO 쿠키정보 (Cookie named LPINFO)
+		$lpenc->set("a_id", $LPINFO);					//LPINFO 쿠키정보 (Cookie named LPINFO)
 		$lpenc->set("m_id", merchant_id);			//머천트 ID(Merchant ID in Linkprice system)
 		$lpenc->set("mbr_id", user_id);				//실적 발생 회원 ID(User ID who make business recorde in merchant system )
 		$lpenc->set("o_cd", order_code);			//주문코드(Order number)
