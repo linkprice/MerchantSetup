94438b34a0b9fb994f314c405c8167470da02738
shji-linkprice <shji@linkprice.com>
2017-08-22 13:08:37 +0900

    주석 수정

--- CPA/PHP/daily_fix.php
+++ CPA/PHP/daily_fix.php
@@ -4,28 +4,33 @@ define(NWNAME, "linkprice"); // network name value
 $daily_fix = array();
 
 //Do not change alias name
-$sql = "select	network_value 		a_id,
-                order_time          order_time,
-				user_id 			member_id,
-				order_code 			order_code,
-				'member' 		    product_code,
-				'FREE' 			    category_code,
-				remote_address 		remote_addr,
-				u_agent 			user_agent
-		from your_order_table
-		where yyyymmdd = ?
-		and	  network_name = ?";
+$sql = "select	network_value		lpinfo,
+                order_time		order_time,
+		user_id			member_id,
+		order_code 		unique_id,
+		'registration'		action,
+		'FREE'			category_code,
+		'ë¬´ë£Œ íšŒì› ê°€ìž…'	   action_name,
+		remote_address 		remote_addr,
+		u_agent			user_agent
+	from your_order_table
+	where yyyymmdd = ?
+	and network_name = ?";
 
 $stmt = mysqli_stmt_init($conn);
 if(mysqli_stmt_prepare($stmt,$sql)){
     mysqli_stmt_bind_param($stmt,"ss",$_REQUEST["yyyymmdd"],NWNAME);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    $daily_fix = mysqli_fetch_assoc($result);
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        $daily_fix[] = $row;
+    }
+    	
     mysqli_stmt_close($stmt);
 }
 
 echo json_encode($daily_fix);
 
-// DataBase Á¢¼Ó ²÷±â
+// DataBase Close
 ?>
