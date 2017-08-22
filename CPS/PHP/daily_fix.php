aa42ff0e98cd3a31799415399fb46e83fb3ccd82
shji-linkprice <shji@linkprice.com>
2017-08-22 12:54:55 +0900

    Update daily_fix.php

--- CPS/PHP/daily_fix.php
+++ CPS/PHP/daily_fix.php
@@ -41,9 +41,8 @@ if(mysqli_stmt_prepare($stmt,$sql)){
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 	
-    $send_data = array();
     while ($row = mysqli_fetch_assoc($result)) {
-        $send_data[] = $row;
+        $daily_fix[] = $row;
     }
 	
     mysqli_stmt_close($stmt);
