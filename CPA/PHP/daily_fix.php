ffe37e14b9c9a29ba8b29ef7ad569dbcb452bd7e
kimjs <kimjs@linkprice.com>
2018-03-13 09:24:58 +0900

    Update daily_fix.php
    $conn 객체 생성 루틴 추가

--- CPA/PHP/daily_fix.php
+++ CPA/PHP/daily_fix.php
@@ -1,7 +1,12 @@
 <?php
-
 define(NWNAME, "linkprice"); // network name value
 $daily_fix = array();
+
+$db_ip = "DATABASE IP";
+$db_id = "DATABASE ID";
+$db_pw = "DATABASE PASSWORD";
+$db_nm = "DATABASE NAME";
+$conn = mysqli_connect($db_ip, $db_id, $db_pw,$db_nm);
 
 //Do not change alias name
 $sql = "select	network_value		lpinfo,
