6dd97e25119984dabb192faad1e5c8ab12b876d7
kimjs <kimjs@linkprice.com>
2018-03-16 17:26:23 +0900

    Update auto_cancel.php

--- CPS/PHP/auto_cancel.php
+++ CPS/PHP/auto_cancel.php
@@ -7,25 +7,24 @@ $db_pw = "DATABASE PASSWORD";
 $db_nm = "DATABASE NAME";
 $conn = mysqli_connect($db_ip, $db_id, $db_pw, $db_nm);
 
-$sql = "select	'order_status' = 
-                CASE 
-                when order_status = '미입급' then 0 
-                when order_status = '확정' then 1
-                when order_status = '주문취소' then 2
-                when order_status = '주문없음' then 3
-                else 9
-                END,
-                'reason' = 
-                CASE
-                when order_status = '미입급' then '미입금' 
-                when order_status = '확정' then '주문 최종 확정'
-                when order_status = '주문취소' then '주문 취소'
-                when order_status = '주문없음' then '주문 없음'
-                else '확인요망(예외상황')
-                END          
-		from your_order_table
-		where order_code = ?
-		and	  product_code = ?";
+$sql = "SELECT	
+	CASE 
+	WHEN	order_status = '미입급' THEN 0 
+	WHEN	order_status = '확정' THEN 1
+	WHEN	order_status = '주문취소' THEN 2
+	WHEN	order_status = '주문없음' THEN 3
+	ELSE	9
+	END AS `order_status`,
+	CASE
+	WHEN 	order_status = '미입급' THEN '미입금' 
+	WHEN	order_status = '확정' THEN '주문 최종 확정'
+	WHEN	order_status = '주문취소' THEN '주문 취소'
+	WHEN	order_status = '주문없음' THEN '주문 없음'
+	ELSE '확인요망(예외상황')
+	END AS `reason`
+	FROM	your_order_table
+	WHERE	order_code = ?
+	AND	product_code = ?";
 
 $stmt = mysqli_stmt_init($conn);
 if(mysqli_stmt_prepare($stmt,$sql)){
