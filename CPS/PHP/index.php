1560ce38003b703d7fc024676ced39de2ea62dec
kimjs <kimjs@linkprice.com>
2018-03-19 13:48:35 +0900

    Update index.php

--- CPS/PHP/index.php
+++ CPS/PHP/index.php
@@ -46,7 +46,6 @@ if(mysqli_stmt_prepare($stmt,$sql)){
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
  
-    $send_data = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $send_data[] = $row;
     }	
